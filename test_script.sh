ae6a4a7a0a95aab61842ee1f83d1733db06346f6
lnicosia <lnicosia@student.42.fr>
2022-05-24 11:58:15 +0200

    [ADD] Test script cleaner and also tests return values.

--- test_script.sh
+++ test_script.sh
@@ -2,8 +2,12 @@
 
 OPT="$@"
 
-echo "$file diff:"
 ./ft_ls $OPT > ft_ls.txt
+echo "$?" > ft_ls-ret.txt
 ls $OPT > ls.txt
+echo "$?" > ls-ret.txt
+echo "Output diff:"
 diff ft_ls.txt ls.txt
+echo "Return diff:"
+diff ft_ls-ret.txt ls-ret.txt
 #rm ft_ls.txt ls.txt
